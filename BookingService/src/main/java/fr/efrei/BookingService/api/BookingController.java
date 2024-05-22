6ba35732d06017b958cc8ac32dcb644b8021b902
nom2 <nom2@email.com>
2024-05-22 22:33:46 +0200

    avancement d de la booking api

--- BookingService/src/main/java/fr/efrei/BookingService/api/BookingController.java
+++ BookingService/src/main/java/fr/efrei/BookingService/api/BookingController.java
@@ -1,5 +1,6 @@
 package fr.efrei.BookingService.api;
 
+import fr.efrei.BookingService.services.BookingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,26 +8,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/booking")
-/*
-    * Pour essayer ce code, vous pouvez utiliser le lien suivant:
-    * http://localhost:8080/booking
-    * http://localhost:8080/booking/TestRecord
-    * http://localhost:8080/booking/Message/ToutCeQueVousVoulez
-*/
+/* 8081 */
+
 public class BookingController {
+
+    private BookingService bookingService;
+
+    public BookingController() {
+        this.bookingService = new BookingService();
+    }
 
     @GetMapping
     public String getBookings() {
-        return "Booking and now a new message !";
+        return "Get available bookings";
     }
 
     @GetMapping("/Message/{bookingId}")
     public String getBooking(@PathVariable String bookingId) {
-        return "Booking " + bookingId;
+        return "Booking" + bookingId;
     }
 
     @GetMapping("/TestRecord")
-    public BookingRecord testRecord() {
-        return new BookingRecord("monIdDeProprio", "monIdDeLocataire", "monIdDeTenant");
+    public BookingResponse testRecord() {
+        return new BookingResponse("monIdDeProprio", "monIdDeLocataire", "monIdDeTenant", "debutResa", "finResa");
+    }
+    @GetMapping("/Cancel/{bookingId}")
+    public void cancel(@PathVariable String bookingId){
+        bookingService.cancel(bookingId);
+    }
+
+    @GetMapping("/Book/{bookingId}")
+    public BookingResponse book (String bookingId, String estateId, String tenantId, String bookingStart, String bookingEnd){
+        //BookingResponse reponse = new BookingResponse(bookingId, estateId, tenantId, bookingStart, bookingEnd);
+        bookingService.book(bookingId, estateId, tenantId, bookingStart, bookingEnd);
     }
 }
--- BookingService/src/main/java/fr/efrei/BookingService/services/BookingService.java
+++ BookingService/src/main/java/fr/efrei/BookingService/services/BookingService.java
@@ -3,15 +3,16 @@ package fr.efrei.BookingService.services;
 import fr.efrei.BookingService.core.BookingEntity;
 
 public class BookingService {
-    public BookingEntity book(String estateId, String tenantId) {
+    public BookingEntity book(String bookingId, String estateId, String tenantId, String bookingStart, String bookingEnd) {
         /* Check for availability */
         /* Create a booking from DAO */
         /* return it */
-        return new BookingEntity("bookingId", estateId, tenantId);
+        return new BookingEntity("bookingId", estateId, tenantId, bookingStart, bookingEnd);
     }
 
     public void cancel(String bookingId) {
         /* Check for existence */
         /* Delete the booking from DAO */
+
     }
 }
