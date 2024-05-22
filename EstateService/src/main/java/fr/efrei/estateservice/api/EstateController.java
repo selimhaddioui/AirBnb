1787f01fa07df5585751e3587a2d7b6520475d54
Sanaa <sanaa.abd19@yahoo.fr>
2024-05-22 22:51:06 +0200

    avancement estate service:

--- EstateService/src/main/java/fr/efrei/estateservice/api/EstateController.java
+++ EstateService/src/main/java/fr/efrei/estateservice/api/EstateController.java
@@ -1,39 +1,26 @@
-package fr.efrei.estateservice.api;
+package fr.efrei.EstateService.API;
 
-import fr.efrei.estateservice.service.EstateService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
+@RequestMapping
+
 public class EstateController {
-    private final EstateService estateService;
-
-    public EstateController(EstateService estateService) {
-        this.estateService = estateService;
-    }
-
     @GetMapping
-    public String ping() {
-        return "Hello on Estate Service" ;
+    public String publishEstate() {
+        return "coucou";
     }
 
-    @GetMapping("/all")
-    public List<EstateResponse> getAllEstate() {
-        return estateService.getEstates().stream().map(EstateResponse::new).toList();
+    @GetMapping("/Message/{estateId}")
+    public String getEstate(@PathVariable String EstateId) {
+        return "Booking " + EstateId;
     }
 
-    @GetMapping("/new/{id}")
-    public String createEstate(@PathVariable String id) {
-        estateService.createEstate(id);
-        return "Go to http://localhost:8082/all to see your new record" ;
-    }
-
-    @GetMapping("/delete/{id}")
-    public String deleteEstate(@PathVariable String id) {
-        estateService.deleteEstate(id);
-        return "Go to http://localhost:8082/all to see your new record" ;
+    @GetMapping("/TestRecord")
+    public EstateRecord testRecord() {
+        return new EstateRecord("monIdDeProprio", "monIdDeLocataire");
     }
 }
